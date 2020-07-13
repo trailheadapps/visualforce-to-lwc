50f370b0053ce057a61c0d5187eb8554c4056794
pozil <philippe.ozil@gmail.com>
2020-07-13 15:41:38 +0200

    Added tests for page* wrappers

---
+++ force-app/main/default/lwc/paginatedListWrapper/__tests__/paginatedListWrapper.test.js
@@ -0,0 +1,25 @@
+import { createElement } from 'lwc';
+import PaginatedListWrapper from 'c/paginatedListWrapper';
+
+describe('c-page-messages-toast-wrapper', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('shows inner component using c-paginated-list-wrapper', () => {
+        // Create initial element
+        const element = createElement('c-paginated-list-wrapper', {
+            is: PaginatedListWrapper
+        });
+        document.body.appendChild(element);
+
+        const exampleEl = element.shadowRoot.querySelector('c-example-wrapper');
+        expect(exampleEl).not.toBeNull();
+
+        const innerEl = exampleEl.querySelector('c-paginated-list');
+        expect(innerEl).not.toBeNull();
+    });
+});
