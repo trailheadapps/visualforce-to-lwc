4f859ebfe47f199cd6a24f083dfbd2ba0c2fe1fe
Alba Rivas <arivas@salesforce.com>
2020-07-13 15:29:54 +0200

    Tests for editRecord (#11)
    * Add mocks for schema and ShowToastEvent
    Add tests for editRecord
    
    * Added constant for recordId
    Divide tests in suites for success and error case
    
    * Fix typo when creating object
    
    * Use verb in singular

---
+++ force-app/test/jest-mocks/schema.js
@@ -0,0 +1,7 @@
+export const Account = {
+    objectApiName: 'Account',
+    Name: { fieldApiName: 'Name' },
+    Type: { fieldApiName: 'Type' },
+    Type: { fieldApiName: 'Phone' },
+    NumberOfEmployees: { fieldApiName: 'NumberOfEmployees' }
+};
