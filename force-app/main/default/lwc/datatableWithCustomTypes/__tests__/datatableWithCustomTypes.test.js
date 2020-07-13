e69fbda22c3f2e7fed8b3e69305cddc64de6b89d
Alba Rivas <arivas@salesforce.com>
2020-07-13 15:30:24 +0200

    Tests for DatatableWithCustomTypes (#10)
    * Add tests for DatatableWithCustomTypes

---
+++ force-app/main/default/lwc/datatableWithCustomTypes/__tests__/datatableWithCustomTypes.test.js
@@ -0,0 +1,22 @@
+import DatatableWithCustomTypes from 'c/datatableWithCustomTypes';
+import navigateToRecordTemplate from '../navigateToRecordTemplate.html';
+
+describe('c-datatable-with-custom-types', () => {
+    it('defines a navigateToRecord custom type for lightning-datatable', () => {
+        expect(
+            DatatableWithCustomTypes.customTypes.navigateToRecord.template
+        ).toBe(navigateToRecordTemplate);
+        expect(
+            DatatableWithCustomTypes.customTypes.navigateToRecord.typeAttributes
+                .length
+        ).toBe(2);
+        expect(
+            DatatableWithCustomTypes.customTypes.navigateToRecord
+                .typeAttributes[0]
+        ).toBe('recordId');
+        expect(
+            DatatableWithCustomTypes.customTypes.navigateToRecord
+                .typeAttributes[1]
+        ).toBe('label');
+    });
+});
