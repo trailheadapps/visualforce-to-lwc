d5264dc78c3352c50b4827a82bcc445772601bd5
Alba Rivas <arivas@salesforce.com>
2020-07-13 13:20:35 +0200

    Tests for  basicListGetListUiWrapper (#5)
    * Add tests for basicGetListUiWrapper
    * Include mock that the test needs

---
+++ force-app/main/default/lwc/basicListGetListUiWrapper/__tests__/basicListGetListUiWrapper.tests.js
@@ -0,0 +1,29 @@
+import { createElement } from 'lwc';
+import BasicListGetListUiWrapper from 'c/basicListGetListUiWrapper';
+
+// Mock inner component, as it uses a wire adapter and crashes
+jest.mock('../../basicListGetListUi/basicListGetListUi');
+
+describe('c-basic-list-get-list-ui-wrapper', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+    it('shows inner component using c-example-wrapper', () => {
+        // Create initial element
+        const element = createElement('c-basic-list-get-list-ui-wrapper', {
+            is: BasicListGetListUiWrapper
+        });
+        document.body.appendChild(element);
+
+        const exampleCmp = element.shadowRoot.querySelector(
+            'c-example-wrapper'
+        );
+        expect(exampleCmp).not.toBeNull();
+
+        const innerCmp = exampleCmp.querySelector('c-basic-list-get-list-ui');
+        expect(innerCmp).not.toBeNull();
+    });
+});
