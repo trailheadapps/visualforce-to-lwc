bc0f95f441e40e9006587489eabbf73eeeb8c6a0
Alba Rivas <arivas@salesforce.com>
2020-07-13 12:17:31 +0200

    Add jest config file and tests for apexUtils (#4)
    Add jest config file
    Add tests for apexUtils module

---
+++ force-app/main/default/lwc/apexUtils/__tests__/apexUtils.test.js
@@ -0,0 +1,19 @@
+import { formatApexSObjects } from 'c/apexUtils';
+
+// Realistic data after a calling SOQL in Apex
+const soqlData = require('./data/soqlData.json');
+
+describe('c-apex-utils', () => {
+    it('flattens apex sobjects', () => {
+        // Create initial element
+        const result = formatApexSObjects(soqlData);
+
+        result.forEach((record, index) => {
+            // Record fields remain the same
+            expect(record.Name).toBe(soqlData[index].Name);
+            // Related record fields are flatenned
+            expect(record['Owner.Id']).toBe(soqlData[index].Owner.Id);
+            expect(record['Owner.Name']).toBe(soqlData[index].Owner.Name);
+        });
+    });
+});
