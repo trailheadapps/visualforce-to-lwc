1802a256f9112b46395960568148241725e1f307
Philippe Ozil <5071767+pozil@users.noreply.github.com>
2020-07-14 09:04:43 +0200

    Added tests for 4 page* wrappers (#13)
    * Added tests for page* wrappers
    
    * Fixed test descriptions

---
+++ force-app/main/default/lwc/pageBlocksWrapper/__tests__/pageBlocksWrapper.test.js
@@ -0,0 +1,25 @@
+import { createElement } from 'lwc';
+import PageBlocksWrapper from 'c/pageBlocksWrapper';
+
+describe('c-page-blocks-wrapper', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('shows inner component using c-example-wrapper', () => {
+        // Create initial element
+        const element = createElement('c-page-blocks-wrapper', {
+            is: PageBlocksWrapper
+        });
+        document.body.appendChild(element);
+
+        const exampleEl = element.shadowRoot.querySelector('c-example-wrapper');
+        expect(exampleEl).not.toBeNull();
+
+        const innerEl = exampleEl.querySelector('c-page-blocks');
+        expect(innerEl).not.toBeNull();
+    });
+});
---
+++ force-app/main/default/lwc/pageMessagesFormWrapper/__tests__/pageMessagesFormWrapper.test.js
@@ -0,0 +1,25 @@
+import { createElement } from 'lwc';
+import PageMessagesFormWrapper from 'c/pageMessagesFormWrapper';
+
+describe('c-page-messages-form-wrapper', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('shows inner component using c-example-wrapper', () => {
+        // Create initial element
+        const element = createElement('c-page-messages-form-wrapper', {
+            is: PageMessagesFormWrapper
+        });
+        document.body.appendChild(element);
+
+        const exampleEl = element.shadowRoot.querySelector('c-example-wrapper');
+        expect(exampleEl).not.toBeNull();
+
+        const innerEl = exampleEl.querySelector('c-page-messages-form');
+        expect(innerEl).not.toBeNull();
+    });
+});
