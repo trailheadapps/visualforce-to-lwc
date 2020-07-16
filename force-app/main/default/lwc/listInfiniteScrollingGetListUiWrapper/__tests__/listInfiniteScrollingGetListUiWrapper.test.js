f03f98607a4f69d3eced7b314ed7e887456d73cc
Heather Dykstra <missheatherlaree@gmail.com>
2020-07-16 09:12:26 -0600

    List Infinite Scrolling Get List Ui Wrapper Test

---
+++ force-app/main/default/lwc/listInfiniteScrollingGetListUiWrapper/__tests__/listInfiniteScrollingGetListUiWrapper.test.js
@@ -0,0 +1,34 @@
+import { createElement } from 'lwc';
+import ListInfiniteScrollingGetListUiWrapper from 'c/listInfiniteScrollingGetListUiWrapper';
+
+// Mock inner component, as it uses a wire adapter and crashes
+jest.mock(
+    '../../listInfiniteScrollingGetListUi/listInfiniteScrollingGetListUi'
+);
+
+describe('c-list-infinite-scrolling-get-list-ui-wrapper', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+    it('shows inner component using c-example-wrapper', () => {
+        // Create initial element
+        const element = createElement(
+            'c-list-infinite-scrolling-get-list-ui-wrapper',
+            {
+                is: ListInfiniteScrollingGetListUiWrapper
+            }
+        );
+        document.body.appendChild(element);
+
+        const exampleEl = element.shadowRoot.querySelector('c-example-wrapper');
+        expect(exampleEl).not.toBeNull();
+
+        const innerEl = exampleEl.querySelector(
+            'c-list-infinite-scrolling-get-list-ui'
+        );
+        expect(innerEl).not.toBeNull();
+    });
+});
