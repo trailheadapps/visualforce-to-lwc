f48abe9fcf66d2365f109ebdeb663173263a03f3
Peter Chittum <pchittum@salesforce.com>
2020-07-16 14:31:30 +0100

    test for tabsWrapper component

---
+++ force-app/main/default/lwc/tabsWrapper/__tests__/tabsWrapper.test.js
@@ -0,0 +1,25 @@
+import { createElement } from 'lwc';
+import TabsWrapper from 'c/tabsWrapper';
+
+describe('c-tabs-wrapper', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('shows inner component using c-tabs-wrapper', () => {
+        const element = createElement('c-tabs-wrapper', {
+            is: TabsWrapper
+        });
+        document.body.appendChild(element);
+
+        // Verify required child components are present on render.
+        const exampleEl = element.shadowRoot.querySelector('c-example-wrapper');
+        expect(exampleEl).not.toBeNull();
+
+        const innerEl = exampleEl.querySelector('c-tabs');
+        expect(innerEl).not.toBeNull();
+    });
+});
