f2984a3f9bb2203c77736e02e967b46681f92223
Peter Chittum <pchittum@salesforce.com>
2020-07-16 14:38:44 +0100

    added tests for c/toolbarWrapper

---
+++ force-app/main/default/lwc/toolbarWrapper/__tests__/toolbarWrapper.test.js
@@ -0,0 +1,25 @@
+import { createElement } from 'lwc';
+import ToolbarWrapper from 'c/toolbarWrapper';
+
+describe('c-toolbar-wrapper', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('shows inner component using c-toolbar-wrapper', () => {
+        const element = createElement('c-toolbar-wrapper', {
+            is: ToolbarWrapper
+        });
+        document.body.appendChild(element);
+
+        // Verify required child components are present on render.
+        const exampleEl = element.shadowRoot.querySelector('c-example-wrapper');
+        expect(exampleEl).not.toBeNull();
+
+        const innerEl = exampleEl.querySelector('c-toolbar');
+        expect(innerEl).not.toBeNull();
+    });
+});
