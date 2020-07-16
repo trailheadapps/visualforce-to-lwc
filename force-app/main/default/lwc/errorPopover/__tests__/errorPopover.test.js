33745d3a54df5d4bfb76989699586dc23cb61b2e
msrivastav13 <mohith.shrivastava@salesforce.com>
2020-07-16 12:24:18 -0400

    resolve comments from pozil

--- force-app/main/default/lwc/errorPopover/__tests__/errorPopover.test.js
+++ force-app/main/default/lwc/errorPopover/__tests__/errorPopover.test.js
@@ -18,42 +18,6 @@ describe('c-error-popover', () => {
         element.errors = ERROR_MESSAGE_INPUT;
         document.body.appendChild(element);
 
-        const sectionEl = element.shadowRoot.querySelector('section');
-        expect(sectionEl).not.toBeNull();
-
-        // check for float right div element
-        const divEl = element.shadowRoot.querySelector('div.slds-float_right');
-        expect(divEl).not.toBeNull();
-
-        // check for lightning button icon element
-        const buttonIconEl = element.shadowRoot.querySelector(
-            'lightning-button-icon'
-        );
-        expect(buttonIconEl).not.toBeNull();
-        expect(buttonIconEl.variant).toBe('bare');
-        expect(buttonIconEl.iconClass).toBe('slds-button_icon-inverse');
-        expect(buttonIconEl.iconName).toBe('utility:close');
-        expect(buttonIconEl.alternativeText).toBe('Close dialog');
-        expect(buttonIconEl.size).toBe('small');
-
-        //check for header element
-        const headerEl = element.shadowRoot.querySelector('header');
-        expect(headerEl).not.toBeNull();
-
-        // check for lightning icon element
-        const lightningIconEl = element.shadowRoot.querySelector(
-            'lightning-icon'
-        );
-        expect(lightningIconEl).not.toBeNull();
-        expect(lightningIconEl.variant).toBe('inverse');
-        expect(lightningIconEl.iconName).toBe('utility:error');
-        expect(lightningIconEl.alternativeText).toBe('Error');
-        expect(lightningIconEl.size).toBe('x-small');
-
-        // check for the header element title
-        const headerTitleEl = element.shadowRoot.querySelector('h2');
-        expect(headerTitleEl.textContent).toBe('Resolve error');
-
         //check for errors in final div
         const popoverDivEl = element.shadowRoot.querySelector(
             'div.slds-var-popover__body'
@@ -61,7 +25,7 @@ describe('c-error-popover', () => {
         expect(popoverDivEl.textContent).toBe(ERROR_MESSAGE_INPUT);
     });
 
-    it('clicks close Dialog buton', () => {
+    it('hides dialog when close button is clicked', () => {
         const ERROR_MESSAGE_INPUT = 'Friendly Error Message';
 
         const element = createElement('c-error-popover', {
@@ -70,14 +34,8 @@ describe('c-error-popover', () => {
         element.errors = ERROR_MESSAGE_INPUT;
         document.body.appendChild(element);
 
-        const sectionEl = element.shadowRoot.querySelector('section');
-        expect(sectionEl).not.toBeNull();
-
         // click lightning button element
-        const buttonIconEl = element.shadowRoot.querySelector(
-            'lightning-button-icon'
-        );
-        buttonIconEl.click();
+        element.shadowRoot.querySelector('lightning-button-icon').click();
 
         return Promise.resolve().then(() => {
             // check that the section is not rendered once close dialog button is clicked.
@@ -97,10 +55,6 @@ describe('c-error-popover', () => {
         element.errors = ERROR_MESSAGE_INPUT;
         document.body.appendChild(element);
 
-        const sectionEl = element.shadowRoot.querySelector('section');
-        expect(sectionEl).not.toBeNull();
-
-        // call toggle function to hide errorPopover element
         element.toggle();
 
         return Promise.resolve().then(() => {
