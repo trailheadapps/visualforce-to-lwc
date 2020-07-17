14f1ec954c327fab42dd09d690d303b856a6108f
Kevin Poorman <codefriar@users.noreply.github.com>
2020-07-17 13:50:55 -0400

    Prerelease/navigate to record tests (#43)
    * Adds Tests for NavigateToRecord

---
+++ force-app/main/default/lwc/navigateToRecord/__tests__/navigateToRecord.test.js
@@ -0,0 +1,49 @@
+import { createElement } from 'lwc';
+import NavigateToRecord from 'c/navigateToRecord';
+import { getNavigateCalledWith } from 'lightning/navigation';
+
+describe('c-navigate-to-record', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('navigates to record view by ID', () => {
+        const element = createElement('c-navigate-to-record', {
+            is: NavigateToRecord
+        });
+        element.label = 'foo';
+        element.recordId = '0031700000pJRRTAA4';
+        document.body.appendChild(element);
+
+        return Promise.resolve().then(() => {
+            // Get handle to anchor and fire click event
+            const anchorEl = element.shadowRoot.querySelector('a');
+            expect(anchorEl).not.toBeNull();
+            anchorEl.click();
+
+            const { pageReference } = getNavigateCalledWith();
+
+            expect(pageReference.type).toEqual('standard__recordPage');
+            expect(pageReference.attributes.recordId).toEqual(element.recordId);
+            expect(pageReference.attributes.actionName).toEqual('view');
+        });
+    });
+
+    it('creates an anchor element with the right label', () => {
+        const element = createElement('c-navigate-to-record', {
+            is: NavigateToRecord
+        });
+        element.label = 'foo';
+        document.body.appendChild(element);
+
+        return Promise.resolve().then(() => {
+            // Get handle to anchor and fire click event
+            const anchorEl = element.shadowRoot.querySelector('a');
+            expect(anchorEl).not.toBeNull();
+            expect(anchorEl.textContent).toEqual(element.label);
+        });
+    });
+});
