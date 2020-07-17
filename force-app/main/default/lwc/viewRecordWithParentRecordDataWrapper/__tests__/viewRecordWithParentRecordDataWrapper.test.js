35a53a1ed1e4f586e1c426669047d0fca3757b78
muenzpraeger <mail@winkelmeyer.com>
2020-07-17 11:04:40 +0200

    feat: add test for viewRecordWithParentRecordDataWrapper

---
+++ force-app/main/default/lwc/viewRecordWithParentRecordDataWrapper/__tests__/viewRecordWithParentRecordDataWrapper.test.js
@@ -0,0 +1,50 @@
+import { createElement } from 'lwc';
+import ViewRecordWithParentRecordDataWrapper from 'c/viewRecordWrapper';
+
+const RECORD_ID = '0019A00000E8zAWQAZ';
+
+describe('c-view-record-with-parent-record-data-wrapper', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('passes recordId to c-example-wrapper component', () => {
+        // Create initial element
+        const element = createElement(
+            'c-view-record-with-parent-record-data-wrapper',
+            {
+                is: ViewRecordWithParentRecordDataWrapper
+            }
+        );
+        element.recordId = RECORD_ID;
+        document.body.appendChild(element);
+
+        // Verify example component exists, and that recordId is passed to its attribute
+
+        const exampleEl = element.shadowRoot.querySelector('c-example-wrapper');
+        expect(exampleEl).not.toBeNull();
+        expect(exampleEl.recordId).toBe(RECORD_ID);
+    });
+
+    it('passes recordId to c-view-record-with-parent-data component', () => {
+        // Create initial element
+        const element = createElement(
+            'c-view-record-with-parent-record-data-wrapper',
+            {
+                is: ViewRecordWithParentRecordDataWrapper
+            }
+        );
+        element.recordId = RECORD_ID;
+        document.body.appendChild(element);
+
+        // Verify c-view-record-with-parent-record-data component exists, and that recordId is passed to its attribute
+        const viewRecordParentEl = element.shadowRoot.querySelector(
+            'c-view-record-with-parent-record-data'
+        );
+        expect(viewRecordParentEl).not.toBeNull();
+        expect(viewRecordParentEl.recordId).toBe(RECORD_ID);
+    });
+});
