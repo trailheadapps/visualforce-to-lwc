3c31155f29a05d8c442fe998221f6d75fc94d1ef
msrivastav13 <mohith.shrivastava@salesforce.com>
2020-07-17 17:58:55 -0400

    try this to see if it fixes error

--- force-app/main/default/lwc/errorPopover/__tests__/errorPopover.test.js
+++ force-app/main/default/lwc/errorPopover/__tests__/errorPopover.test.js
@@ -19,10 +19,16 @@ describe('c-error-popover', () => {
         document.body.appendChild(element);
 
         //check for errors in final div
-        const popoverDivEl = element.shadowRoot.querySelector(
-            'div.slds-var-popover__body'
-        );
-        expect(popoverDivEl.textContent).toBe(ERROR_MESSAGE_INPUT);
+        return Promise.resolve().then(() => {
+            const reRenderedSectionEl = element.shadowRoot.querySelector(
+                'section'
+            );
+            expect(reRenderedSectionEl).not.toBeNull();
+            const popoverDivEl = reRenderedSectionEl.querySelector(
+                'div.slds-var-popover__body'
+            );
+            expect(popoverDivEl.textContent).toBe(ERROR_MESSAGE_INPUT);
+        });
     });
 
     it('hides dialog when close button is clicked', () => {
