b879dbf83e29c30fa267c2be4dfdaa7743553612
Kevin Poorman <codefriar@users.noreply.github.com>
2020-07-22 10:59:49 -0400

    Adds Jest tests for PageBlocks LWC Component (#52)
    * Adds Jest tests for PageBlocks LWC Component

---
+++ force-app/main/default/lwc/pageBlocks/__tests__/pageBlocks.test.js
@@ -0,0 +1,25 @@
+import { createElement } from 'lwc';
+import PageBlocks from 'c/pageBlocks';
+
+describe('c-page-block', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+    it('displays lightning-accordion-sections A and B', () => {
+        const element = createElement('c-page-blocks', {
+            is: PageBlocks
+        });
+        document.body.appendChild(element);
+
+        return Promise.resolve().then(() => {
+            const accordionEl = element.shadowRoot.querySelector(
+                'lightning-accordion'
+            );
+            expect(accordionEl).not.toBeNull();
+            expect(accordionEl.activeSectionName).toStrictEqual(['A', 'B']);
+        });
+    });
+});
