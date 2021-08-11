3fae0b5320a62a6a10be4019f734c0af272309a9
Aditya Naag <atopalli@salesforce.com>
2021-08-11 22:31:16 +0530

    renderedCallback (after render) recipe  (#179)
    * Connected callback recipe
    
    * Removed extra whitespace in link
    
    * Added new recipe for post render operations
    
    * Minor test script fix
    
    * Undo wrong conflict fix
    
    * Add cmp to page
    
    Co-authored-by: Alba Rivas <arivas@salesforce.com>
    Co-authored-by: Philippe Ozil <5071767+pozil@users.noreply.github.com>

---
+++ force-app/main/default/lwc/afterRenderHookWrapper/__tests__/afterRenderHookWrapper.test.js
@@ -0,0 +1,38 @@
+import { createElement } from 'lwc';
+import AfterRenderHookWrapper from 'c/afterRenderHookWrapper';
+
+// Mock exampleWrapper component so that accessibility tests don't expect the visualforce iframe to load
+jest.mock('../../exampleWrapper/exampleWrapper');
+
+describe('c-after-render-hook-wrapper', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('shows inner component using c-example-wrapper', () => {
+        // Create initial element
+        const element = createElement('c-after-render-hook-wrapper', {
+            is: AfterRenderHookWrapper
+        });
+        document.body.appendChild(element);
+
+        const exampleEl = element.shadowRoot.querySelector('c-example-wrapper');
+        expect(exampleEl).not.toBeNull();
+
+        const innerEl = exampleEl.querySelector('c-after-render-hook');
+        expect(innerEl).not.toBeNull();
+    });
+
+    it('is accessible', async () => {
+        const element = createElement('c-after-render-hook-wrapper', {
+            is: AfterRenderHookWrapper
+        });
+
+        document.body.appendChild(element);
+
+        await expect(element).toBeAccessible();
+    });
+});
